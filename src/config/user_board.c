cf3a0e1b666e748266fcc499354f658e5e135029
VrakasLabs <tim.vrakas@gmail.com>
2018-09-11 00:01:46 -0500

    It almost works! This needs the latest arm-none-eabi-gcc to be coppied into PlatformIO for it to compile right...

---
+++ src/config/user_board.c
@@ -0,0 +1,14 @@
+#include <user_board.h>
+#include <asf.h>
+
+void system_board_init(void){
+//Setup LED Pin
+struct port_config p;
+port_get_config_defaults(&p);
+p.direction = PORT_PIN_DIR_OUTPUT;
+port_pin_set_config(PIN_PA17, &p);
+
+//Setup USB Serial
+stdio_usb_init();
+stdio_usb_enable();
+}
